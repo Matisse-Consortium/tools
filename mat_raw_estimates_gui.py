9cc91b67cd5826dca3d2d8eb91f1a54f61f0fc27
fmillour <fmillour@520a3bef-24c0-4980-83cc-2114d36be6d4>
2017-04-10 12:13:40 +0000

    Update header with GPL license Add the possibility to set output directory, with auto completion
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@58 520a3bef-24c0-4980-83cc-2114d36be6d4

--- mat_raw_estimates_gui.py
+++ mat_raw_estimates_gui.py
@@ -1,20 +1,44 @@
 #!/usr/bin/python  
 # -*- coding: utf-8 -*-
 """
+  $Id:  $
+ 
+  This file is part of the Matisse pipeline GUI series
+  Copyright (C) 2017- Observatoire de la Côte d'Azur
+ 
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ 
 Created on Wed Apr  5 10:18:07 2017
 
-@author: fmillour
+  $Author: fmillour $
+  $Date:  $
+  $Revision:  $
+  $Name:  $
 """
+
 import wx
 import os
 import pwd
 import errno
 
 fileTypes = ["HOT_DARK", "CALIB_SRC_RAW", "BADPIX", "OBS_FLATFIELD",
-             "SHIFT_MAP", "NONLINEARITY", "KAPPA_MATRIX"]
+             "NONLINEARITY", "SHIFT_MAP", "KAPPA_MATRIX"]
 
 mainPath = ""
-sofFile = ""
+sofFile  = ""
+runDir   = ""
 row = 0;
 col = 0;
 guiTitle = "mat_raw_estimates";
@@ -33,6 +57,7 @@ class displayGui(wx.Frame):
         sizer = wx.GridBagSizer(3, len(fileTypes))
                          
         # Define the main buttons with the file selection here
+               # input files part 
         self.btnDark = tributton(panel, sizer, fileTypes[0], 1)
         self.btnRaw  = tributton(panel, sizer, fileTypes[1], 1)
         self.btnFlat = tributton(panel, sizer, fileTypes[2], 1)
@@ -44,33 +69,53 @@ class displayGui(wx.Frame):
         
         # Buttons for interactivity are set here
                 
-        btnOpenSOF = wx.Button(panel, label='Open SOF')
+               # SOF file part 
         col = 0
         row += 1
+        btnOpenSOF = wx.Button(panel, label='Open SOF')
         sizer.Add(btnOpenSOF, (row, col))
         btnOpenSOF.Bind(wx.EVT_BUTTON, self.OnOpenSOF)
+        
+        col = 1
+        self.btnDescSOF  = wx.StaticText(panel, label="SOF file", name="dirRun")
+        sizer.Add(self.btnDescSOF, (row, col))
                 
         col = 2
         self.btnTextSOF = wx.TextCtrl  (panel, size=(400, -1),style=wx.TE_RICH)
         sizer.Add(self.btnTextSOF, (row, col))
         self.btnTextSOF.Bind(wx.EVT_TEXT, self.OnTextSOF)
                 
-        btnSOF     = wx.Button(panel, label='Generate SOF')
         col = 4
+        btnSOF     = wx.Button(panel, label='Generate SOF')
         sizer.Add(btnSOF, (row, col))
         btnSOF.Bind(wx.EVT_BUTTON, self.OnGenSOF)
-        
-        
-        btnOK = wx.Button(panel, label='Cancel', size=(60, -1))
+                
+               # Run part
         col = 0
         row += 1
-        sizer.Add(btnOK, (row, col))
-        btnOK.Bind(wx.EVT_BUTTON, self.OnClose)
+        btnMagic = wx.Button(panel, label='auto set')
+        sizer.Add(btnMagic, (row, col))
+        btnMagic.Bind(wx.EVT_BUTTON, self.OnOpenMagic)
+        
+        col = 1
+        self.btnDescRun  = wx.StaticText(panel, label="output dir", name="dirRun")
+        sizer.Add(self.btnDescRun, (row, col))
                 
-        btnRun = wx.Button(panel, label='Run!')
+        col = 2
+        self.btnTextRun = wx.TextCtrl  (panel, size=(400, -1),style=wx.TE_RICH)
+        sizer.Add(self.btnTextRun, (row, col))
+        self.btnTextRun.Bind(wx.EVT_TEXT, self.OnTextRun)
+                
         col = 4
+        btnRun = wx.Button(panel, label='Run!')
         sizer.Add(btnRun, (row, col))
         btnRun.Bind(wx.EVT_BUTTON, self.OnRunRex)
+                
+        col = 0
+        row += 1
+        btnOK = wx.Button(panel, label='Cancel', size=(60, -1))
+        sizer.Add(btnOK, (row, col))
+        btnOK.Bind(wx.EVT_BUTTON, self.OnClose)
          
         # Set simple sizer for a nice border
         border = wx.BoxSizer()
@@ -86,6 +131,31 @@ class displayGui(wx.Frame):
         global sofFile
        # print("yeah man!")     
         sofFile  = self.btnTextSOF.GetValue()   
+        
+    def OnTextRun(self,e):
+        global runDir
+       # print("yeah man!")     
+        runDir  = self.btnTextRun.GetValue()   
+        
+    def OnOpenMagic(self,e):
+        global sofFile, runDir
+        print("Hocus Pocus, abracadabra et tutti quanti !")
+            
+            # Test default directory
+        for idx, btn in enumerate(self.btns):
+            if btn.filelist:
+                path = os.path.dirname(btn.filelist[0])
+                if btn.typeFile == fileTypes[0] or btn.typeFile == fileTypes[1]:
+                    runDir = path+"../Results/";
+                    
+            # Otherwise use SOF file directory
+        if sofFile != "":
+            runDir = os.path.dirname(sofFile)+"/../Results/";
+            
+        print("Set the output path to"+runDir)
+        # Expand environment variables
+        runDir = os.path.expandvars(runDir)
+        self.btnTextRun.SetValue(runDir)
         
     def OnOpenSOF(self,e):
         global mainPath, sofFile, waitforupdate
@@ -123,17 +193,20 @@ class displayGui(wx.Frame):
         
         for idx, btn in enumerate(self.btns):
             if btn.filelist:
-                mainPath = os.path.dirname(btn.filelist[0])
-                # Expand environment variables
-                mainPath = os.path.expandvars(mainPath)
-                btn.dirtxt.SetValue(mainPath)
+                path = os.path.dirname(btn.filelist[0])
+                if btn.typeFile == fileTypes[0] or btn.typeFile == fileTypes[1]:
+                    mainPath = path
+                    print("Set the main path to"+mainPath)
+                    # Expand environment variables
+                    mainPath = os.path.expandvars(mainPath)
+                btn.dirtxt.SetValue(path)
                 # Get file list
                 mystring = [os.path.basename(fil) for fil in btn.filelist]
                 text = mystring[0];
                 for texti in mystring[1:]:
                     text += ", "+texti
-               # print("What's in the file")
-               # print(text)
+               #     print("What's in the file")
+               #     print(text)
                 btn.filetxt.SetValue(text)
             
         waitforupdate = 0;
@@ -169,6 +242,14 @@ class displayGui(wx.Frame):
                             
     def OnRunRex(self,e):
         global guiTitle
+        global runDir
+        if runDir!="":
+            self.make_sure_path_exists(runDir)
+            os.chdir(runDir)
+        else:
+            wx.MessageBox('Please set an output path first', 'Error', wx.OK | wx.ICON_ERROR)
+            return -1
+            
         command = "esorex "+guiTitle+" "+sofFile+"&";
         print(command)
         os.system(command)
@@ -245,6 +326,9 @@ class tributton(wx.Frame):
             text = self.dirtxt.GetValue()
             if not os.path.isdir(text):
                 self.dirtxt.SetBackgroundColour(wx.RED)
+            if self.typeFile == fileTypes[1]:
+                mainPath = os.path.dirname(self.filelist[0])
+                print("Set the main path to"+mainPath)
                 flagnosave = 1;
             else:
                 self.dirtxt.SetBackgroundColour(wx.WHITE)
@@ -305,9 +389,12 @@ class tributton(wx.Frame):
             print "You chosed the following file(s):"
             print self.filelist
         
-            mainPath = os.path.dirname(self.filelist[0])
+            path = os.path.dirname(self.filelist[0])
+            if self.typeFile == fileTypes[0] or self.typeFile == fileTypes[1]:
+                mainPath = path
+                print("Set the main path to"+mainPath)
                 
-            self.dirtxt.SetValue(mainPath)
+            self.dirtxt.SetValue(path)
             # Get file list
             mystring = [os.path.basename(fil) for fil in self.filelist]
             text = mystring[0];
