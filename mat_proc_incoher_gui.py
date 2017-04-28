e3629dbdf82c2e23fd51a2732dedfe5801c2570b
fmillour <fmillour@520a3bef-24c0-4980-83cc-2114d36be6d4>
2017-04-28 16:11:35 +0000

    add mat_proc_incoher
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@75 520a3bef-24c0-4980-83cc-2114d36be6d4

---
+++ mat_proc_incoher_gui.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python  
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  5 10:18:07 2017
+
+@author: fmillour
+"""
+import wx
+import os
+
+
+
+
+#!/usr/bin/python  
+# -*- coding: utf-8 -*-
+"""
+  Created on Wed Apr 12 12:04:01 2017
+
+@author: ame
+"""
+
+import wx
+from mat_generic_gui import displayGui
+
+
+fileTypes = ["OBJ_CORR_FLUX", "PHOT_BEAMS", "OI_OPDWVPO"]
+checkPresent = [1,1,0]
+             
+GuiTitle="mat_proc_incoher"
+
+class mat_raw_estimates_gui(displayGui):
+    def __init__(self):
+          super(mat_raw_estimates_gui, self).__init__(GuiTitle,".",fileTypes, checkPresent)
+       
+
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = mat_raw_estimates_gui()
+    frame.Show()
+    app.MainLoop()
+    app.Destroy()
