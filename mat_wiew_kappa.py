00ef3dcf5e4359f07e7d204af40607a1353263d5
pbe <pbe@520a3bef-24c0-4980-83cc-2114d36be6d4>
2017-08-22 07:58:21 +0000

    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@77 520a3bef-24c0-4980-83cc-2114d36be6d4

--- mat_wiew_kappa.py
+++ mat_wiew_kappa.py
@@ -10,12 +10,13 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from matplotlib.backends.backend_pdf import PdfPages
 
-hdulist = fits.open('C:\Users\pbe\Desktop\Python-TestPlan\KAPPA_N_LOW.fits')
+hdulist = fits.open('C:\Users\pbe\Desktop\Doc-TestPlan\kappa_N_low.fits')
 
 nBeam=4
 regName=[]
 szSpectral=[]
 coefKappa=[]
+errCoefKappa=[]
 cornerSpectral=[]
 for i in range(nBeam):
     iReg=hdulist['KAPPA_MATRIX'].data['REGION'][i]
@@ -23,6 +24,7 @@ for i in range(nBeam):
     regName.append(hdulist['IMAGING_DETECTOR'].data['REGNAME'][ix[0]][0])
     szSpectral.append(hdulist['IMAGING_DETECTOR'].data['NAXIS'][ix[0]][0][1])
     coefKappa.append(hdulist['KAPPA_MATRIX'].data['MATRIX'][i])
+    errCoefKappa.append(hdulist['KAPPA_MATRIX'].data['ERROR'][i])
     cornerSpectral.append(hdulist['IMAGING_DETECTOR'].data['CORNER'][ix[0]][0][1])
     
 detName=hdulist[0].header['HIERARCH ESO DET CHIP NAME']
@@ -49,29 +51,55 @@ hdulist.close()
 f, axarr = plt.subplots(3, sharex=True, figsize=(8,8))
 f.suptitle('KAPPA MATRIX',fontsize=20)
 for i in range(nBeam):
-    axarr[0].plot(wavelength,coefKappa[i][0:szSpectral[i]],label=regName[i],marker=(4,i,0),linestyle='')
+#    axarr[0].plot(wavelength,coefKappa[i][0:szSpectral[i]],label=regName[i],marker=(4,i,0),linestyle='-')
+     #axarr[0].plot(wavelength,coefKappa[i][0:szSpectral[i]],label=regName[i],linestyle='-')
+    axarr[0].errorbar(wavelength,coefKappa[i][0:szSpectral[i]],yerr=errCoefKappa[i][0:szSpectral[i]],label=regName[i],linestyle='-')
 axarr[0].plot([wavelength[0],wavelength[szSpectral[0]-1]],[1.5,1.5],linestyle='--',color='black')
 axarr[0].plot([wavelength[0],wavelength[szSpectral[0]-1]],[3,3],linestyle='--',color='black')
-axarr[0].set_ylim([1,3.5])
+if (wavelength[szSpectral[0]-1] > 10):
+    axarr[0].plot([8,8],[0,4.5],linestyle='--',color='black')
+    axarr[0].plot([13,13],[0,4.5],linestyle='--',color='black')
+else :
+    axarr[0].plot([2.8,2.8],[0,4.5],linestyle='--',color='black')
+    axarr[0].plot([5,5],[0,4.5],linestyle='--',color='black')
+axarr[0].set_ylim([0,4.5])
 axarr[0].set_xlim([wavelength[0],wavelength[szSpectral[0]-1]])
 axarr[0].set_ylabel('Ratio')
 axarr[0].set_title(configType,fontsize=12)
 
 for i in range(nBeam):
-    axarr[1].plot(wavelength,coefKappa[i][szSpectral[i]:2*szSpectral[i]],label=regName[i],marker=(4,i,0),linestyle='')
-axarr[1].set_ylim([5,7])
+    print np.mean(coefKappa[i][szSpectral[i]+85:szSpectral[i]+135])
+    print np.std(coefKappa[i][szSpectral[i]+85:szSpectral[i]+135])
+#    axarr[1].plot(wavelength,coefKappa[i][szSpectral[i]:2*szSpectral[i]],label=regName[i],marker=(4,i,0),linestyle='-')
+    #axarr[1].plot(wavelength,coefKappa[i][szSpectral[i]:2*szSpectral[i]],label=regName[i],linestyle='-')
+    axarr[1].errorbar(wavelength,coefKappa[i][szSpectral[i]:2*szSpectral[i]],yerr=errCoefKappa[i][szSpectral[i]:2*szSpectral[i]],label=regName[i],linestyle='-')
+if (wavelength[szSpectral[0]-1] > 10):
+    axarr[1].plot([8,8],[1,11],linestyle='--',color='black')
+    axarr[1].plot([13,13],[1,11],linestyle='--',color='black')
+else :
+    axarr[1].plot([2.8,2.8],[1,11],linestyle='--',color='black')
+    axarr[1].plot([5,5],[1,11],linestyle='--',color='black')
+axarr[1].set_ylim([1,11])
 axarr[0].set_xlim([wavelength[0],wavelength[szSpectral[0]-1]])
 #axarr[1].legend(prop={'size':8},loc=2)
 axarr[1].set_ylabel('Zoom')
 
 for i in range(nBeam):
-    axarr[2].plot(wavelength,coefKappa[i][2*szSpectral[i]:3*szSpectral[i]],label=regName[i],marker=(4,i,0),linestyle='')
-axarr[2].set_ylim([-0.1,0.1])
+#    axarr[2].plot(wavelength,coefKappa[i][2*szSpectral[i]:3*szSpectral[i]],label=regName[i],marker=(4,i,0),linestyle='-')
+    #axarr[2].plot(wavelength,coefKappa[i][2*szSpectral[i]:3*szSpectral[i]],label=regName[i],linestyle='-')
+    axarr[2].errorbar(wavelength,coefKappa[i][2*szSpectral[i]:3*szSpectral[i]],yerr=errCoefKappa[i][2*szSpectral[i]:3*szSpectral[i]],label=regName[i],linestyle='-')
+if (wavelength[szSpectral[0]-1] > 10):
+    axarr[2].plot([8,8],[-100,100],linestyle='--',color='black')
+    axarr[2].plot([13,13],[-100,100],linestyle='--',color='black')
+else :
+    axarr[2].plot([2.8,2.8],[-100,100],linestyle='--',color='black')
+    axarr[2].plot([5,5],[-100,100],linestyle='--',color='black')
+axarr[2].set_ylim([-30,20])
 axarr[0].set_xlim([wavelength[0],wavelength[szSpectral[0]-1]])
 axarr[2].legend(prop={'size':8},loc=1)
 axarr[2].set_ylabel('Shift')
 axarr[2].set_xlabel('Wavelength ($\mu$m)')
 
-pp=PdfPages('foo.pdf')
+pp=PdfPages('C:\Users\pbe\Desktop\Doc-TestPlan\kappa_N_low.pdf')
 pp.savefig(f)
 pp.close()
