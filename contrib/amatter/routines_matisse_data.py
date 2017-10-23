1cc8a5cac9f5be282ee92ee76777132a2f00605a
amatter <amatter@520a3bef-24c0-4980-83cc-2114d36be6d4>
2017-10-23 09:03:53 +0000

    minor changes in the 'loda data' routines
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@101 520a3bef-24c0-4980-83cc-2114d36be6d4

--- contrib/amatter/routines_matisse_data.py
+++ contrib/amatter/routines_matisse_data.py
@@ -2,6 +2,7 @@
 """
 Routines of general use for MATISSE data reduction
 Created on Thu Jan 12 2017
+Last version from Mon Oct 23 2017
 @author: ama
 """
 
@@ -55,10 +56,10 @@ def load_data_raw(filename):
     return data 
     
 
-def load_data_cal(filename):
-#------------------------------------------------------------------------------------------------------------
-#Routine to load MATISSE fringe data from fits files produced by the mat_cal_image (after detector cosmetics)
-#------------------------------------------------------------------------------------------------------------
+def load_data_cal_Siphot(filename):
+#---------------------------------------------------------------------------------------------------------------------------
+#Routine to load MATISSE fringe data (Si_phot mode) from fits files produced by the mat_cal_image (after detector cosmetics)
+#---------------------------------------------------------------------------------------------------------------------------
 
 #filename: MATISSE raw data file (template format) after detector cosmetics
 
@@ -90,7 +91,46 @@ def load_data_cal(filename):
     #Debug plot of the data frame
     #plt.figure()
     #plt.imshow(data[0,:,:],vmin=0)
-    return data_sub 
+    return data_sub,nf,dimy,dimx
+    
+    
+
+def load_data_cal_Highsens(filename):
+#---------------------------------------------------------------------------------------------------------------------------
+#Routine to load MATISSE fringe data (High_Sens mode) from fits files produced by the mat_cal_image (after detector cosmetics)
+#---------------------------------------------------------------------------------------------------------------------------
+
+#filename: MATISSE raw data file (template format) after detector cosmetics
+
+    hdulist = fits.open(filename)
+    img = hdulist['IMAGING_DATA'].data['DATA1']
+    nframe=np.size(img,axis=0)
+    nf=nframe/2
+    dimy=np.size(img,axis=1)
+    dimx=np.size(img,axis=2)
+    data=np.zeros([nf,dimy,dimx],dtype=np.float)
+    data_bg=np.zeros([nf,dimy,dimx],dtype=np.float)
+    data_sub=np.zeros([nf,dimy,dimx],dtype=np.float)
+    
+    #Extraction of the raw frames
+    for i in range(nf):
+        data[i,:,:] = hdulist['IMAGING_DATA'].data['DATA1'][i]
+    hdulist.close()
+    
+    #Extraction of the background frames
+    for i in range(nf):
+        data_bg[i,:,:] = hdulist['IMAGING_DATA'].data['DATA1'][i+nf]
+    hdulist.close()  
+    
+    #Subtraction of the background frames
+    dark_mean=np.mean(data_dark,axis=0)
+    for i in range(nf):
+        data_sub[i,:,:]=data[i,:,:]-dark_mean
+    
+    #Debug plot of the data frame
+    #plt.figure()
+    #plt.imshow(data[0,:,:],vmin=0)
+    return data_sub,nf,dimy,dimx 
 
     
        
@@ -147,7 +187,7 @@ def computeFft1D_spectral(interf,shift_im_min,shift_im_max,dimx,dimy,lam_tab,lam
                 chi2_shift_min=chi2_shift[l,k]
                 shift_ima[k]=a[l]
     shift_ima_mean=np.mean(shift_ima)
-    print("shift_ima_mean = {0}").format(shift_ima_mean)
+    #print("shift_ima_mean = {0}").format(shift_ima_mean)
     #print("shift_ima = {0}").format(shift_ima)
     
     #Centering of the interferometric frame by applying the correct numerical shift 'shift_ima_mean'
@@ -164,8 +204,8 @@ def computeFft1D_spectral(interf,shift_im_min,shift_im_max,dimx,dimy,lam_tab,lam
         #plt.figure()
         #plt.plot(np.sqrt(np.abs(fft1D_dub)))
         #plt.plot(np.real(fft1D[10,:]))
-        #plt.figure()
-        #plt.plot(np.abs(fft1D[200,:]))
+    #plt.figure()
+    #plt.plot(np.abs(fft1D[200,:]))
     return fft1D
       
             
@@ -494,256 +534,3 @@ def computePistons_1D(pistons,phasor1D,sigma_real,sigma_imag,lam_tab,dimy):
     
     
 
-#def computePistons_1D_spectral_wv(piston,phasor1D,sigma_real,sigma_imag,lam_tab,dimy):
-##-----------------------------------------------------------------------------------------------------------------------------
-##Routine to compute the OPD (piston) associated with the differential complex phasor computed with the spectral method. 
-##This piston is determined through a fit (over all the spectral channels) with a quadratic phasor model. The chi2 minimization 
-##is performed by scanning a grid of piston values and finding the piston value giving the lowest chi2 value.  
-##-----------------------------------------------------------------------------------------------------------------------------    
-#    
-##pistons: grid of piston values (in um) for the chi2 minimization (parameter space for the fit)
-##phasor1D: differential phasor computed with the spectral method
-##sigma_real: uncertainty on the real part of the differential complex phasor
-##sigma_imag: uncertainty on the imaginary part of the differential complex phasor
-##lam_tab: wavelength array
-##dimy: dimension of the 1D FFT in the spectral direction
-#
-#    pistons = np.arange(piston-15.0,piston+15.0,0.01,dtype=np.float)
-#    npistons=np.size(pistons)
-#    a = np.arange(-70.0,-49.0,0.5,dtype=np.float)
-#    dima=np.size(a)
-#    chi2_phasor1D=np.zeros([npistons,dima],dtype=float)   
-#    chi2_min=1e12
-#    for i in range(dima):
-#        for j in range(npistons):
-#            phasor1D_mod=np.exp(-1j*2.0*np.pi*(pistons[j]*(1./lam_tab-1./lam_tab[np.int(dimy/2)])+a[i]*(np.square(1./lam_tab)-np.square(1./lam_tab[np.int(dimy/2)]))))
-#            phasor_tot=phasor1D*phasor1D_mod
-#            sigma2_var=np.square(sigma_real)+np.square(sigma_imag)                
-#            chi2_phasor1D[j,i]=np.sum(np.square(np.imag(phasor_tot))/sigma2_var)
-#            if (chi2_phasor1D[j,i] < chi2_min):
-#                chi2_min=chi2_phasor1D[j,i]
-#                j_min=j
-#                i_min=i  
-#    piston1D=pistons[j_min]
-#    a1D=a[i_min]
-#    phasor1D_mod_min=np.exp(1j*2.0*np.pi*(piston1D*(1./lam_tab-1./lam_tab[np.int(dimy/2)])+a1D*(np.square(1./lam_tab)-np.square(1./lam_tab[np.int(dimy/2)]))))
-#    sigma2_piston1D=1./(np.sum((1./sigma2_var)*np.square(np.real(phasor1D)*np.real(phasor1D_mod_min)-np.imag(phasor1D)*np.imag(phasor1D_mod_min))))
-#    phasor_tot_min=(phasor1D*phasor1D_mod_min)
-#    sigma2_var_min=np.square(sigma_real*np.imag(phasor1D_mod_min))+np.square(sigma_imag*np.real(phasor1D_mod_min))  
-#            
-#    #plt.figure()        
-#    #plt.plot(lam_tab,np.imag(phasor1D)/np.abs(phasor1D))    
-#    #plt.plot(lam_tab,np.imag(phasor1D_mod_min))    
-#    #plt.figure()        
-#    #plt.plot(lam_tab,np.real(phasor1D)/np.abs(phasor1D))    
-#    #plt.plot(lam_tab,np.real(phasor1D_mod_min))           
-#    #plt.figure()    
-#    #plt.plot(1./lam_tab,np.angle(phasor1D_mod_min))
-#    #plt.plot(1./lam_tab,np.angle(phasor1D))
-#    #plt.figure()
-#    #plt.plot(a,chi2_phasor1D[j_min,:])    
-#    #plt.plot(lam_tab,np.square(np.imag(phasor_tot_min)))
-#    #plt.plot(lam_tab,sigma2_var_min)
-#    return(piston1D,sigma2_piston1D,phasor1D_mod_min,phasor_tot_min)
-
-#
-##-----------------------------------------------------------------------------------------------------------------------------
-##Routine to compute the OPD (piston) associated with the absolute complex phasor computed with the computePhasors_1D routine. 
-##This piston is determined through a fit (over all the spectral channels) with a quadratic phasor model. The chi2 minimization 
-##is performed by scanning a grid of piston values and finding the piston value giving the lowest chi2 value.  
-##-----------------------------------------------------------------------------------------------------------------------------      
-#def computePistons_1D_wv(piston,phasor1D,sigma_real,sigma_imag,lam_tab,dimy):
-##pistons: grid of piston values (in um) for the chi2 minimization (parameter space for the fit)
-##phasor1D: Absolute phasor provided by the computePhasors_1D routine
-##sigma_real: uncertainty on the real part of the differential complex phasor
-##sigma_imag: uncertainty on the imaginary part of the differential complex phasor
-##lam_tab: wavelength array
-##dimy: dimension of the 1D FFT in the spectral direction
-# 
-#    pistons = np.arange(piston-15.0,piston+15.0,0.01,dtype=np.float)
-#    npistons=np.size(pistons)
-#    a = np.arange(-70.0,-49.0,0.5,dtype=np.float)
-#    dima=np.size(a)
-#    chi2_phasor1D=np.zeros([npistons,dima],dtype=float)   
-#    chi2_min=1e12
-#    for i in range(dima):
-#        for j in range(npistons):
-#            phasor1D_mod=np.exp(-1j*2.0*np.pi*(pistons[j]*(1./lam_tab)+a[i]*(np.square(1./lam_tab))))
-#            phasor_tot=phasor1D*phasor1D_mod
-#            sigma2_var=np.square(sigma_real)+np.square(sigma_imag)                
-#            chi2_phasor1D[j,i]=np.sum(np.square(np.imag(phasor_tot))/sigma2_var)
-#            if (chi2_phasor1D[j,i] < chi2_min):
-#                chi2_min=chi2_phasor1D[j,i]
-#                j_min=j
-#                i_min=i  
-#    piston1D=pistons[j_min]
-#    a1D=a[i_min]
-#    phasor1D_mod_min=np.exp(1j*2.0*np.pi*(piston1D*(1./lam_tab)+a1D*(np.square(1./lam_tab))))
-#    sigma2_piston1D=1./(np.sum((1./sigma2_var)*np.square(np.real(phasor1D)*np.real(phasor1D_mod_min)-np.imag(phasor1D)*np.imag(phasor1D_mod_min))))
-#    phasor_tot_min=(phasor1D*phasor1D_mod_min)
-#    sigma2_var_min=np.square(sigma_real*np.imag(phasor1D_mod_min))+np.square(sigma_imag*np.real(phasor1D_mod_min)) 
-#            
-#    #plt.figure()        
-#    #plt.plot(lam_tab,np.imag(phasor1D)/np.abs(phasor1D))    
-#    #plt.plot(lam_tab,np.imag(phasor1D_mod_min))    
-#    #plt.figure()        
-#    #plt.plot(lam_tab,np.real(phasor1D)/np.abs(phasor1D))    
-#    #plt.plot(lam_tab,np.real(phasor1D_mod_min))
-#    #plt.figure()    
-#    #plt.plot(1./lam_tab,np.angle(phasor1D_mod_min))
-#    #plt.plot(1./lam_tab,np.angle(phasor1D))
-#    #plt.figure()
-#    #plt.plot(pistons,chi2_phasor1D)    
-#    #plt.plot(lam_tab,np.square(np.imag(phasor_tot_min)))
-#    #plt.plot(lam_tab,sigma2_var_min)
-#    return(piston1D,sigma2_piston1D,phasor1D_mod_min,phasor_tot_min) 
-#
-#
-#    
-#    
-#def fit_plot_piston(dirdat,dirplot,fileForth,fileBack,loc_legend):
-##    
-#    xForth,pistonForth,pistonForthErr = np.loadtxt(fileForth)
-#    xBack,pistonBack,pistonBackErr = np.loadtxt(fileBack)
-#    
-#    div    = fileForth.split("/")
-##    #dstep  = div[-3]
-#    #device = div[-2]
-#    device = div[-2].split("_")[0]
-#    filename_plot0=div[-1].split(".")[0].split("_")[0]
-#    filename_plot1=div[-1].split(".")[0].split("_")[1]
-#    #Fit de la loi voltage/piston
-#    aForth,covForth  = np.polyfit(xForth,pistonForth,1,w=pistonForthErr,cov=True)
-#    yForth           = aForth[0]*xForth+aForth[1]
-#    
-#    aBack,covBack  = np.polyfit(xBack,pistonBack,1,w=pistonBackErr,cov=True)
-#    yBack           = aBack[0]*xBack+aBack[1]
-#    
-#    xGlobal=np.append(xForth,xBack)
-#    pistonGlobal=np.append(pistonForth,pistonBack)
-#    pistonGlobalErr=np.append(pistonForthErr,pistonBackErr)
-#    aGlobal,covGlobal  = np.polyfit(xGlobal,pistonGlobal,1,w=pistonGlobalErr,cov=True)
-#    yGlobal           = aGlobal[0]*xGlobal+aGlobal[1]    
-#    
-#    #Print of the results
-#    print("Forth: a = {0:2.4f}$\pm${1:2.4f}".format(aForth[0],np.sqrt(covForth[0,0])))
-#    print("Forth; b = {0:2.4f}$\pm${1:2.4f}".format(aForth[1],np.sqrt(covForth[1,1])))
-#    print("Back: a = {0:2.4f}$\pm${1:2.4f}".format(aBack[0],np.sqrt(covBack[0,0])))
-#    print("Back; b = {0:2.4f}$\pm${1:2.4f}".format(aBack[1],np.sqrt(covBack[1,1])))
-#    print("Global: a = {0:2.4f}$\pm${1:2.4f}".format(aGlobal[0],np.sqrt(covGlobal[0,0])))
-#    print("Global; b = {0:2.4f}$\pm${1:2.4f}".format(aGlobal[1],np.sqrt(covGlobal[1,1])))
-#
-##    
-#    #Plot de la loi voltage/piston pour direction Forth et Back
-#    fig  = plt.figure()
-#    gs   = gridspec.GridSpec(3, 1)
-#    plt1 = fig.add_subplot(gs[0:2,:])
-#    plt.title("Calibration of Piezo-actuator {0}".format(device)) 
-#    plt.ylabel(r"Measured piston ($\mu$m)")
-#    dot_Forth,    = plt.plot(xForth,pistonForth, 'ro')
-#    line_Forth,   = plt.plot(xForth,yForth,'r')
-#    dot_back,  = plt.plot(xBack,pistonBack, 'go')
-#    line_back, = plt.plot(xBack,yBack,'g')
-#    plt.legend([(line_Forth,dot_Forth), (line_back,dot_back)], ['Forth $\Rightarrow${0:2.4f}$\pm${1:2.4f} $\mu$m/Volt'.format(aForth[0],np.sqrt(covForth[0,0])), 'Back $\Rightarrow${0:2.4f}$\pm${1:2.4f} $\mu$m/Volt'.format(aBack[0],np.sqrt(covBack[0,0]))],loc=loc_legend)
-#    plt.legend([(line_Forth,dot_Forth), (line_back,dot_back)], ['Forth $\Rightarrow${0:2.4f}$\pm${1:2.4f} $\mu$m/Volt'.format(aForth[0],np.sqrt(covForth[0,0])), 'Back $\Rightarrow${0:2.4f}$\pm${1:2.4f} $\mu$m/Volt'.format(aBack[0],np.sqrt(covBack[0,0]))],loc=loc_legend)
-##    
-#    plt.tick_params(axis='x',which='both', bottom='off',top='off',labelbottom='off')
-##    
-#    #plt1.set_xticklabels([]);
-#    fig.add_subplot(gs[2,:],sharex=plt1)
-#    plt.plot(xForth,yForth-pistonForth,'ro')
-#    plt.plot(xBack,yBack-pistonBack,'go')
-#    plt.plot([xBack[0],xBack[-1]],[0,0],'k--')
-#    plt.xlabel(r"Piezo Voltage (V)")               
-#    plt.ylabel(r"Residual ($\mu$m)")    
-#    
-#    #Sauvegarde des plots
-#    #fileeps = dirplot+"MATISSE_PiezoCalib_{0}.eps".format(device)
-#    #plt.savefig(fileeps)
-#    #filepng = dirplot+"MATISSE_PiezoCalib_{0}.png".format(device)
-#    #plt.savefig(filepng)
-#    fileeps = dirplot+filename_plot0+"_"+filename_plot1+"_{0}.eps".format(device)
-#    plt.savefig(fileeps)
-#    filepng = dirplot+filename_plot0+"_"+filename_plot1+"_{0}.png".format(device)
-#    plt.savefig(filepng)
-#    
-#    #Plot de la loi voltage/piston global
-#    fig  = plt.figure()
-#    gs   = gridspec.GridSpec(3, 1)
-#    plt1 = fig.add_subplot(gs[0:2,:])
-#    plt.title("Calibration of Piezo-actuator {0}".format(device)) 
-#    plt.ylabel(r"Measured piston ($\mu$m)")
-#    dot_Global,    = plt.plot(xGlobal,pistonGlobal, 'ro')
-#    line_Global,   = plt.plot(xGlobal,yGlobal,'r')
-#    plt.legend([(line_Global,dot_Global)], ['Global $\Rightarrow${0:2.4f}$\pm${1:2.4f} $\mu$m/Volt'.format(aGlobal[0],np.sqrt(covGlobal[0,0]))],loc=loc_legend)
-#    plt.legend([(line_Global,dot_Global)], ['Global $\Rightarrow${0:2.4f}$\pm${1:2.4f} $\mu$m/Volt'.format(aGlobal[0],np.sqrt(covGlobal[0,0]))],loc=loc_legend)
-##    
-#    plt.tick_params(axis='x',which='both', bottom='off',top='off',labelbottom='off')
-##    
-#    #plt1.set_xticklabels([]);
-#    fig.add_subplot(gs[2,:],sharex=plt1)
-#    plt.plot(xGlobal,yGlobal-pistonGlobal,'ro')
-#    #plt.plot(xBack,yBack-pistonBack,'go')
-#    plt.plot([xGlobal[0],xGlobal[-1]],[0,0],'k--')
-#    plt.xlabel(r"Piezo Voltage (V)")
-#    plt.ylabel(r"Residual ($\mu$m)")    
-#    
-#    #Sauvegarde des plots
-#    #fileeps_Global = dirplot+"MATISSE_PiezoCalib_Global_{0}.eps".format(device)
-#    #plt.savefig(fileeps_Global)
-#    #filepng_Global = dirplot+"MATISSE_PiezoCalib_Global_{0}.png".format(device)
-#    #plt.savefig(filepng_Global)
-#    
-#    fileeps_Global = dirplot+filename_plot0+"_"+filename_plot1+"_Global_{0}.eps".format(device)
-#    plt.savefig(fileeps_Global)
-#    filepng_Global = dirplot+filename_plot0+"_"+filename_plot1+"_Global_{0}.png".format(device)
-#    plt.savefig(filepng_Global)
-#       
-#    
-#    #Sauvegarde des résidus du fit
-#    res_Forth=yForth-pistonForth
-#    tabForth_residual   = np.array([xForth,res_Forth])
-#    div_forth=fileForth.split("/")
-#    fileforth_residual=dirdat+"residual_"+div_forth[-1]
-#    np.savetxt(fileforth_residual,tabForth_residual)
-#    res_Back=yBack-pistonBack
-#    tabBack_residual   = np.array([xBack,res_Back])
-#    div_back=fileBack.split("/")
-#    fileback_residual=dirdat+"residual_"+div_back[-1]
-#    np.savetxt(fileback_residual,tabBack_residual)
-#    
-#    
-#    #Sauvegarde des plots des histogrammes des résidus du fit
-#    plt.figure()
-#    plt.hist(res_Forth,bins=10)    
-#    fileeps = dirplot+filename_plot0+"_"+filename_plot1+"_res_Forth_{0}.eps".format(device)
-#    plt.savefig(fileeps)
-#    filepng = dirplot+filename_plot0+"_"+filename_plot1+"_res_Forth_{0}.png".format(device)
-#    plt.savefig(filepng)
-#    
-#    plt.figure()
-#    plt.hist(res_Back,bins=10)    
-#    fileeps = dirplot+filename_plot0+"_"+filename_plot1+"_res_Back_{0}.eps".format(device)
-#    plt.savefig(fileeps)
-#    filepng = dirplot+filename_plot0+"_"+filename_plot1+"_res_Back _{0}.png".format(device)
-#    plt.savefig(filepng)
-#    
-#    print("Forth: std deviation = {0}".format(np.std(yForth-pistonForth)))
-#    print("Back: std deviation = {0}".format(np.std(yBack-pistonBack)))
-#    print("Global: std deviation = {0}".format(np.std(yGlobal-pistonGlobal)))    
-#    
-#    
-#def save_piston(filename,step,piston,piston_err):
-#        #Ecriture des pistons dans un fichier
-#        tab = np.array([step,piston,piston_err])
-#        #print("stepBack = {0}".format(self.stepBack))
-#        np.savetxt(filename,tab)
-#        print("Saving piston data to {0}".format(filename))
-#
-#def save_piston_res(filename_res,step,piston_res):
-#        #Ecriture des pistons dans un fichier
-#        tab = np.array([step,piston_res])
-#        #print("stepBack = {0}".format(self.stepBack))
-#        np.savetxt(filename_res,tab)
-#        print("Saving residual piston data to {0}".format(filename_res))
-#
