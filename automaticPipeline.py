c7171333ac1cc808978fb78ddae25654bae71370
pbe <pbe@520a3bef-24c0-4980-83cc-2114d36be6d4>
2017-11-08 10:42:10 +0000

    Sevral log added, parameters added
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@119 520a3bef-24c0-4980-83cc-2114d36be6d4

--- automaticPipeline.py
+++ automaticPipeline.py
@@ -1,33 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-"""
-  $Id:  $
-
-  This file is part of the Matisse pipeline GUI series
-  Copyright (C) 2017- Observatoire de la Côte d'Azur
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
-
-  Created on Fri Oct 27
-
-  $Author:  $
-  $Date: $
-  $Revision:  $
-  $Name:  $
-"""
-
 import numpy as np
 from astropy.io import fits
 import matplotlib.pyplot as plt
@@ -45,31 +15,79 @@ def runEsorex(cmd):
     item=cmd.split()
     out=item[-1]+".log"
     err=item[-1]+".err"
-    print cmd+" > "+out+" 2> "+err
+    print "Recipes",item[2],"running..."
     os.system(cmd+" > "+out+" 2> "+err)
+
+
+
+repRaw=""
+repArchive=""
+repResult=""
+nbCore=0
+listArg=sys.argv
+for elt in listArg:
+    if ('--help' in elt):
+        print "Usage: python automaticPipeline.py --dirRaw=RawDataPath [--dirCalib=CalibrationMapPath] [--dirResult=ResultsPath] [--nbCore=NumberOfCores]"
+        sys.exit(0)
+
+for elt in listArg:
+    if ('--dirRaw' in elt):
+        item=elt.split('=')
+        repRaw=item[1]
+    if ('--dirCalib' in elt):
+        item=elt.split('=')
+        repArchive=item[1]
+    if ('--dirResult' in elt):
+        item=elt.split('=')
+        repResult=item[1]
+    if ('--nbCore' in elt):
+        item=elt.split('=')
+        nbCore=int(item[1])
+print "-----------------------------------------------------------------------------------------"
+if (repRaw == ""):
+    print "ERROR : You have to specifiy a Raw Data Directory with --repRaw=DirectoryPath"
+    sys.exit(0)
+else:
+    print '%-40s' % ("Raw Data Directory:",),repRaw
+if (repArchive==""):   
+        print "Info : Calibration Directory not specified"
+else:
+    print '%-40s' % ("Calibration Directory:",),repArchive
+if (repResult==""):
+        repResult=os.getcwd()
+        print "Info : Results Directory not specified. We use current directory"
+print '%-40s' % ("Results Directory:",),repResult
+if (nbCore==0):
+    nbCore=1
+    print "Info : Number of Cores not specified. We use one core only"
+print '%-40s' % ("Number of Cores:",),nbCore    
+print "-----------------------------------------------------------------------------------------"
+print " "
     
-repRaw='/data-matisse/TransFunc_LowFlux/OTHER'#'/home/pbe/RawAutomatic'
-repResult='/home/pbe/ResultsAutomatic'
-repArchive='/data-matisse/TransFunc_LowFlux/COSMETIC'#'/home/pbe/ArchiveAutomatic'    
+#repRaw='/home/pbe/RawAutomatic'#'/data-matisse/TransFunc_LowFlux/OTHER'#'/home/pbe/RawAutomatic'
+#repResult='/home/pbe/ResultsAutomatic'
+#repArchive='/home/pbe/ArchiveAutomatic'#'/data-matisse/TransFunc_LowFlux/COSMETIC'#'/home/pbe/ArchiveAutomatic'    
 listRaw = [os.path.join(repRaw, f) for f in os.listdir(repRaw) if os.path.isfile(os.path.join(repRaw, f)) and f[-5:] == '.fits']
-listArchive = [os.path.join(repArchive, f) for f in os.listdir(repArchive) if os.path.isfile(os.path.join(repArchive, f)) and f[-5:] == '.fits']
+if (repArchive != ""):
+    listArchive = [os.path.join(repArchive, f) for f in os.listdir(repArchive) if os.path.isfile(os.path.join(repArchive, f)) and f[-5:] == '.fits']
 
 # Determination of the number of Reduction Blocks
 keyTplStart=[]
-
+listIterNumber=[]
 for filename in listRaw:
     hdu=fits.open(filename)
     temp=hdu[0].header['HIERARCH ESO TPL START']+'.'+hdu[0].header['HIERARCH ESO DET CHIP NAME']
     keyTplStart.append(temp)
     hdu.close()
 keyTplStart=list(set(keyTplStart))                       
-
+for elt in keyTplStart:
+    listIterNumber.append(0)
 
 iterNumber=0
 while True:
     iterNumber+=1
     print "Iteration ",iterNumber
-    print " "
+    print "-----------------------"
     if (iterNumber > 1):
         listIter=[]
         for iter in range(iterNumber-1):
@@ -79,10 +97,11 @@ while True:
                 listIter=listIter+[os.path.join(elt, f) for f in os.listdir(elt) if os.path.isfile(os.path.join(elt, f)) and f[-5:] == '.fits']
 
     listRedBlocks=[]
-# Reduction Blocks List Initialization   
+# Reduction Blocks List Initialization  
+    cpt=0
     for elt in keyTplStart:
-           listRedBlocks.append({"action":" ","recipes":" ","param":" ","input":[],"calib":[],"status":0,"tplstart":" "})
-
+        listRedBlocks.append({"action":" ","recipes":" ","param":" ","input":[],"calib":[],"status":0,"tplstart":" ","iter":listIterNumber[cpt]})
+        cpt+=1
 # Fill the list of raw data in the Reduction Blocks List
     for filename in listRaw:
         hdu=fits.open(filename)
@@ -104,13 +123,14 @@ while True:
         elt["tplstart"]=keyTplStartCurrent
 
 # Fill the list of calib in the Reduction Blocks List from repArchive
-    for elt in listRedBlocks:
-        hdu=fits.open(elt["input"][0][0])
-        calib,status=matisseCalib(hdu[0].header,elt["action"],listArchive,elt['calib'])
-        hdu.close()
-        elt["calib"]=calib
-        elt["status"]=status
-        
+    if (repArchive != ""):
+        for elt in listRedBlocks:
+            hdu=fits.open(elt["input"][0][0])
+            calib,status=matisseCalib(hdu[0].header,elt["action"],listArchive,elt['calib'])
+            hdu.close()
+            elt["calib"]=calib
+            elt["status"]=status
+            
 # Fill the list of calib in the Reduction Blocks List from repResult Iter i-1
     if (iterNumber > 1):
         for elt in listRedBlocks:
@@ -129,12 +149,13 @@ while True:
         os.mkdir(repIter)
 
     listCmdEsorex=[]
+    cptStatusOne=0
+    cptStatusZero=0
+    cptToProcess=0
+    cpt=0
     for elt in listRedBlocks:
-        print elt["status"],elt["action"],elt["tplstart"]
-#        print elt['input']
-#        print elt['calib']
-#        print '**********************************************'
         if (elt["status"]==1):
+            cptStatusOne+=1
             sofname=repIter+"/"+elt["recipes"]+"."+elt["tplstart"]+".sof"       
             outputDir=repIter+"/"+elt["recipes"]+"."+elt["tplstart"]+".rb"
             os.mkdir(outputDir)
@@ -153,22 +174,49 @@ while True:
                         #print "copy "+outputDirPrev+" in "+outputDir
                         #os.system("cp "+outputDirPrev+"/* "+outputDir+"/.")
                         shutil.rmtree(outputDir)
-                    else:
+                    else:  
+                        listIterNumber[cpt]=iterNumber
+                        elt["iter"]=iterNumber
+                        cptToProcess+=1
                         listCmdEsorex.append(cmd)
                 else:
+                    listIterNumber[cpt]=iterNumber
+                    elt["iter"]=iterNumber
+                    cptToProcess+=1
                     listCmdEsorex.append(cmd)
             else:
+                listIterNumber[cpt]=iterNumber
+                elt["iter"]=iterNumber
+                cptToProcess+=1
                 listCmdEsorex.append(cmd)
+        else:
+            cptStatusZero+=1
+        cpt+=1
+    print '%40s' % ("Reduction Blocks to process:",),cptToProcess
                 
     if (listCmdEsorex == []):
+        print " "
         print "No more iteration to do"
+        print "-----------------------"
+        print " "
+        print "Processing summary:"
+        print " "
+        for elt in listRedBlocks:
+            if (elt["status"] == 1):
+                msg="Processing done at iteration "+str(elt["iter"])
+            else:
+                if (elt["action"]=="NO-ACTION"):
+                    msg="Data not taken into account by the Pipeline"
+                else:
+                    msg="Reduction Block not processed - Missing calibration"
+            tplstart,detector = elt["tplstart"].split('.')
+            print '%-24s' % (tplstart,),'%-14s' % (detector,),'%-30s' % (elt["action"],),msg
+
         break
     else:
 # Create a process pool with a maximum of 10 worker processes
-        pool = Pool(processes=4)
+        pool = Pool(processes=nbCore)
 # Map our function to a data set - number 1 through 20
         pool.map(runEsorex, listCmdEsorex)
-
-
-
-
+    print '%40s' % ("Reduction Blocks processed:",),cptStatusOne
+    print '%40s' % ("Reduction Blocks not processed:",),cptStatusZero
