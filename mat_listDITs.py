7af679ba60961f05e74fde3eeb5fccc9e10030d0
fmillour <fmillour@520a3bef-24c0-4980-83cc-2114d36be6d4>
2018-03-21 21:03:13 +0000

    Improve script and it gives now DITS for LM & N separately
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@211 520a3bef-24c0-4980-83cc-2114d36be6d4

--- mat_listDITs.py
+++ mat_listDITs.py
@@ -6,6 +6,7 @@ import os
 import numpy as np
 from astropy.io import fits
 import fnmatch
+from libAutoPipeline import matisseType
 
 
 listArg  = sys.argv
@@ -13,49 +14,32 @@ name_dir = sys.argv[1]
 
 files = os.listdir(name_dir)
 
-DITS = []
-
+DITS_LM = []
+DITS_N  = []
+obsTypes = ["TARGET_RAW", "CALIB_RAW", "SKY_RAW"]
 
 for file in files:
     if fnmatch.fnmatch(file,"*.fits*"):
         print(file),
         
         header  = fits.open(os.path.join(name_dir,file))[0].header
-        
-        res  = ""
-        catg = None
-        typ  = None
-        tech = None
-    
-        try:
-            catg=header['HIERARCH ESO PRO CATG']
-        except:
-            try:
-                catg = header['HIERARCH ESO DPR CATG']
-                typ  = header['HIERARCH ESO DPR TYPE']
-                tech = header['HIERARCH ESO DPR TECH']
-            except:
-                pass
+        type = matisseType(header)
 
-        print(catg),
-        print(typ),
-        print(tech),
-            
-        if((catg=="SCIENCE" or catg=="TEST") and typ=="OBJECT" and tech=="INTERFEROMETRY"):
-            res="TARGET_RAW"
-            print(res),
-            DITS = np.append(DITS, header['HIERARCH ESO DET SEQ1 DIT'])
-        elif(catg == "TEST" and typ == "STD" and tech == "INTERFEROMETRY") or (catg == "CALIB" and typ == "OBJECT" and tech == "INTERFEROMETRY") or (catg == "CALIB" and typ == "OBJECT,FLUX" and tech == "INTERFEROMETRY") or (catg == "CALIB" and typ == "STD" and tech == "INTERFEROMETRY"): 
-            res="CALIB_RAW"
-            print(res),
-            DITS = np.append(DITS, header['HIERARCH ESO DET SEQ1 DIT'])
-        elif(catg == "TEST" or catg=="CALIB" or catg=="SCIENCE") and typ == "SKY" and tech == "INTERFEROMETRY": 
-            res="SKY_RAW"
-            print(res),
-            DITS = np.append(DITS, header['HIERARCH ESO DET SEQ1 DIT'])
-        else:
-            res=catg
+        for typ in obsTypes:
+            if type == typ:
+                print(typ),
+                try:
+                    dit = header['HIERARCH ESO DET SEQ1 DIT']
+                except:
+                    pass
+                chip = header["HIERARCH ESO DET CHIP NAME"]
+                if chip == "HAWAII-2RG":
+                    DITS_LM = np.append(DITS_LM, dit)
+                elif chip == "AQUARIUS":
+                    DITS_N  = np.append(DITS_N, dit)
+                
             
         print('')
         
-print(np.unique(DITS))
+print("DITs LM",np.unique(DITS_LM))
+print("DITs N",np.unique(DITS_N))
