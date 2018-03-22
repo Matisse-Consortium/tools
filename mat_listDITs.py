77fce0dd7f4f192a0f7adcf2f6340542d1f94d37
pbe <pbe@520a3bef-24c0-4980-83cc-2114d36be6d4>
2018-03-22 10:04:04 +0000

    Add spectral resolution to the list
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@217 520a3bef-24c0-4980-83cc-2114d36be6d4

--- mat_listDITs.py
+++ mat_listDITs.py
@@ -16,6 +16,8 @@ files = os.listdir(name_dir)
 
 DITS_LM = []
 DITS_N  = []
+RES_LM = []
+RES_N  = []
 obsTypes = ["TARGET_RAW", "CALIB_RAW", "SKY_RAW"]
 
 for file in files:
@@ -29,17 +31,31 @@ for file in files:
             if type == typ:
                 print(typ),
                 try:
-                    dit = header['HIERARCH ESO DET SEQ1 DIT']
+                    dit  = header['HIERARCH ESO DET SEQ1 DIT']
                     chip = header["HIERARCH ESO DET CHIP NAME"]
                 except:
                     pass
                 if chip == "HAWAII-2RG":
-                    DITS_LM = np.append(DITS_LM, dit)
+                    try:
+                        res     = header["HIERARCH ESO INS DIL NAME"]
+                        RES_LM  = np.append(RES_LM, res)
+                        DITS_LM = np.append(DITS_LM, dit)
+                    except:
+                        pass
                 elif chip == "AQUARIUS":
-                    DITS_N  = np.append(DITS_N, dit)
+                    try:
+                        res     = header["HIERARCH ESO INS DIN NAME"]
+                        RES_N   = np.append(RES_N, res)
+                        DITS_N  = np.append(DITS_N, dit)
+                    except:
+                        pass
                 
             
         print('')
-        
-print("DITs LM",np.unique(DITS_LM))
-print("DITs N",np.unique(DITS_N))
+
+res, idx = np.unique(DITS_LM, return_index=True)
+print("DITs LM",res)
+print("RES  LM",RES_LM[idx])
+res, idx = np.unique(DITS_N, return_index=True)
+print("DITs N", res)
+print("RES  N", RES_N[idx])
