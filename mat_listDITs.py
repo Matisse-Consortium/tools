e0cfc4adbbdd580ff523c28c1f3bda781211c54e
fmillour <fmillour@520a3bef-24c0-4980-83cc-2114d36be6d4>
2018-03-23 12:21:56 +0000

    Recursive DIT search + tiny improvements
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@220 520a3bef-24c0-4980-83cc-2114d36be6d4

--- mat_listDITs.py
+++ mat_listDITs.py
@@ -8,54 +8,49 @@ from astropy.io import fits
 import fnmatch
 from libAutoPipeline import matisseType
 
-
-listArg  = sys.argv
-name_dir = sys.argv[1]
-
-files = os.listdir(name_dir)
-
-DITS_LM = []
-DITS_N  = []
 RES_LM = []
-RES_N  = []
+RES_N = []
 obsTypes = ["TARGET_RAW", "CALIB_RAW", "SKY_RAW"]
 
-for file in files:
-    if fnmatch.fnmatch(file,"*.fits*"):
-        print(file),
+for root, dir, files in os.walk("."):
+        print root
+        print ""
+        for file in fnmatch.filter(files, "*.fits*"):
+            print(file),
         
-        header  = fits.open(os.path.join(name_dir,file))[0].header
-        type = matisseType(header)
-
-        for typ in obsTypes:
-            if type == typ:
-                print(typ),
-                try:
-                    dit  = header['HIERARCH ESO DET SEQ1 DIT']
-                    chip = header["HIERARCH ESO DET CHIP NAME"]
-                except:
-                    pass
-                if chip == "HAWAII-2RG":
-                    try:
-                        res     = header["HIERARCH ESO INS DIL NAME"]
-                        RES_LM  = np.append(RES_LM, res)
-                        DITS_LM = np.append(DITS_LM, dit)
-                    except:
-                        pass
-                elif chip == "AQUARIUS":
-                    try:
-                        res     = header["HIERARCH ESO INS DIN NAME"]
-                        RES_N   = np.append(RES_N, res)
-                        DITS_N  = np.append(DITS_N, dit)
-                    except:
-                        pass
-                
+            header  = fits.open(os.path.join(root,file))[0].header
+            type = matisseType(header)
             
-        print('')
+            for typ in obsTypes:
+                if type == typ:
+                    print(typ),
+                    try:
+                        dit  = header['HIERARCH ESO DET SEQ1 DIT']
+                        chip = header["HIERARCH ESO DET CHIP NAME"]
+                        mod  = header["HIERARCH ESO DET READ CURNAME"]
+                    except:
+                        pass
+                    if chip == "HAWAII-2RG":
+                        try:
+                            res     = header["HIERARCH ESO INS DIL NAME"]
+                            strin = chip+" "+res+" "+mod+" "+str(dit)
+                            RES_LM   = np.append(RES_LM, strin)
+                            print(strin)
+                        except:
+                            pass
+                    elif chip == "AQUARIUS":
+                        try:
+                            res     = header["HIERARCH ESO INS DIN NAME"]
+                            strin = chip+" "+res+" "+mod+" "+str(dit)
+                            RES_N   = np.append(RES_N, strin)
+                            print(strin)
+                        except:
+                            pass
+                        
+                        
+            print(' ')
 
-res, idx = np.unique(DITS_LM, return_index=True)
+res, idx = np.unique(RES_LM, return_index=True)
 print("DITs LM",res)
-print("RES  LM",RES_LM[idx])
-res, idx = np.unique(DITS_N, return_index=True)
+res, idx = np.unique(RES_N, return_index=True)
 print("DITs N", res)
-print("RES  N", RES_N[idx])
