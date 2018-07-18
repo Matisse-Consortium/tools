c6792ab907567b0f20281f05588a2906dc51c428
fmillour <fmillour@520a3bef-24c0-4980-83cc-2114d36be6d4>
2018-07-18 19:53:07 +0000

    freezing bug of  fitsheaderviewer.py corrected
    git-svn-id: https://svn.oca.eu/codes/DivMat/DivMat/trunk/python@276 520a3bef-24c0-4980-83cc-2114d36be6d4

--- fitsheaderviewer.py
+++ fitsheaderviewer.py
@@ -16,19 +16,22 @@ class headerKeyword(object):
         self.comment=comment
 
 class fitsHeaderViewer(wx.Frame):
-    def __init__(self,headerOrFilename):
-        super(fitsHeaderViewer, self).__init__(None,title="FitsHeaderViewer",size=(500, 400))  
-       
+    def __init__(self,parent,headerOrFilename):
+        
+        if type(headerOrFilename)==type("") or  type(headerOrFilename)==type(u""):
+            header=fits.getheader(headerOrFilename)
+            filename=headerOrFilename
+        else:
+            header=headerOrFilename 
+            filename = "Unkwnown"
+        wx.Frame.__init__(self, parent, title=filename,size=(800,700))
         self.tableView=ObjectListView(self,wx.ID_ANY, style=wx.LC_REPORT|wx.SUNKEN_BORDER)     
         self.tableView.rowFormatter=self.setRowColor  
-        cols=[ColumnDefn("Keyword","left",150,"keyword",minimumWidth=100),ColumnDefn("Value","left",150,"value",minimumWidth=100),ColumnDefn("Comment","left",150,"comment",minimumWidth=100)]
+        cols=[ColumnDefn("Keyword","left",250,"keyword",minimumWidth=100),ColumnDefn("Value","left",250,"value",minimumWidth=100),ColumnDefn("Comment","left",290,"comment",minimumWidth=100)]
         
         self.tableView.SetColumns(cols)  
 
-        if type(headerOrFilename)==type("") or  type(headerOrFilename)==type(u""):
-            header=fits.getheader(headerOrFilename)
-        else:
-            header=headerOrFilename 
+       
             
         self.keywords=[]
         for keyi in header.keys():
@@ -36,23 +39,9 @@ class fitsHeaderViewer(wx.Frame):
         
   
         self.tableView.SetObjects( self.keywords)
-        
-
-        
-        self.Centre()
-        self.Show()
  
     def setRowColor(self,listItem, data):
        
         c=listItem.GetId() % 2
         listItem.SetBackgroundColour(wx.Colour(250+3*c,250+3*c,250+3*c))
-       
-if __name__ == '__main__':
-    dir0="D:\\Documents\\Travail\\Etoiles\\AMBER_BE\\alpha ara\data\\"
-    filename="PRODUCT_Alpha_Arae_1.89-2.59micron_2007-07-28T05_51_13.5574.fits"
-    app = wx.App()
-    f=fitsHeaderViewer(dir0+filename)
-    f.Show()
-    app.MainLoop()
-
-         
+  
