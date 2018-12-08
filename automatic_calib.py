2f0d5dd09917bbd803e78db4ead80d69a81388b0
fmillour <florentin.millour@oca.eu>
2018-12-08 09:20:35 -0300

    Several improvements of the script: produce one sof file for one science file ; select calibrators in a restricted interval of time around the science target (defaults to 1h) ; rename the product file with a meaningful name.

--- automatic_calib.py
+++ automatic_calib.py
@@ -1,85 +1,140 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-#-----------------------------------------------------------------------------------------
-# Script to run all of the mat_cal_xxx  on the files produced by the automaticPipeline
-#---------- Version 0, 10 Nov 2018 -------------------------------------------------------
-#-----------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# Script to run all of the mat_cal_xxx  on the files produced by the
+#                            automaticPipeline
+#                         Version 0, 10 Nov 2018
+#------------------------------------------------------------------------------
+
 from subprocess import call
 import argparse
 import glob
+import os
+import numpy as np
 from astropy.io import fits
 
-#--------------------------------------------------------------------------------
-#------------ Set up the argument parser for command line arguments -------------
-#---- This really shouldn't take many options besides the config file -----------
-#---- and the input and output directories for data -----------------------------
-#--------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+#------------ Set up the argument parser for command line arguments -----------
+#---- This really shouldn't take many options besides the config file ---------
+#---- and the input and output directories for data ---------------------------
+#------------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description='Wrapper to run the calibration steps of the MATISSE DRL.')
-parser.add_argument('-o', '--out_dir', dest='out_dir', metavar='Output Directory', type=str, default='.', \
-	help='The path to the directory you want results to be stored in (defaults to current directory).')
+
+parser.add_argument('-o', '--out_dir', dest='out_dir', metavar='Output Directory', type=str, default='./CALIBRATED', \
+    help='The path to the directory you want results to be stored in (defaults to current directory).')
 #-------------------------------------------------------------------------
 #--- The SOF should contain TARGET_RAW_INT and CALIB_RAW_INT -------------
 #-------------------------------------------------------------------------
 #parser.add_argument('sof', type=str, help='The filepath containing your sof file. REQUIRED. Should contain TARGET_RAW_INT and CALIB_RAW_INT files.')
-parser.add_argument('--target_dir', dest='targ_dir', metavar='Target Directory', type=str, default='.', \
-	help='The path to the directory containing your TARGET intermediate products. Absolute filepath.')
+parser.add_argument('--in_dir', dest='in_dir', metavar='Working Directory', type=str, default='.', \
+    help='The path to the directory containing your oifits data.')
 
-parser.add_argument('--calib_dir', dest='calib_dir', metavar='Calibrator Directory', type=str, default='.', \
-	help='The path to the directory containing your CALIB intermediate products. Absolute filepath.')
+
+parser.add_argument('--timespan', dest='timespan', metavar='Time span of calibrators', type=str, default='.', \
+    help='The time search interval for selecting calibrators around the science star')
+
 args = parser.parse_args()
 
 
-def make_sof(input_dir, output_dir, targ_type):
-	files =  glob.glob(input_dir+'/*.fits')
-	print input_dir#, files
-	fname ='%s/%s.sof'%(output_dir, targ_type)
-	outfile = open(fname, 'w')
-	for f in files:
-		try:
-			hdu = fits.open(f)
-			obstype = hdu[0].header['pipefile'].split('0')[0]
-			fmat = obstype[:-1]
-			if 'RAW_INT' not in fmat:
-				f = '#' + f
-			outfile.write('{} \t {} \n'.format(f,fmat))
-				#print '{} \t {}'.format(f,fmat), ' added'
-		except:
-			continue
-	outfile.close()
-	return fname
+def make_sof(input_dir, output_dir, timespan=1./24.):
 
+    SOFFILE = [];
 
+    files =  glob.glob(input_dir+'/*.fits')
+    print input_dir#, files
+
+    DIC = []
+    # First read all files
+    print("Reading all files keywords...")
+    for f in files:
+        hdr     = fits.open(f)[0].header
+        dic = {'hdr': hdr}
+        DIC.append(dic)
+        #try:
+    print("Scanning files...")
+    for i,f in enumerate(files):
+            hdri = DIC[i]['hdr']
+            obstype = hdri['ESO PRO CATG']
+
+            if obstype == 'TARGET_RAW_INT':
+                #print("\nFound a TARGET file! Working on it...")
+                print("Working on", f)
+                mjd  = hdri['MJD-OBS']
+                bcd1 = hdri['ESO INS BCD1 NAME']
+                bcd2 = hdri['ESO INS BCD2 NAME']
+                chip = hdri['ESO DET CHIP NAME']
+                dit  = hdri['ESO DET SEQ1 DIT']
+                #print(mjd)
+
+                filename  = os.path.basename(f)
+                name, ext = os.path.splitext(filename)
+
+                #print(filename)
+                #print(name)
+                #print(output_dir)
+                fname ='%s/%s_cal_oifits.sof'%(output_dir, name)
+
+                soffile = open(fname, 'w')
+                SOFFILE.append(fname)
+                soffile.write('{} \t {} \n'.format(f,obstype))
+
+                calcount = 0;
+                for j,fcal in enumerate(files):
+                    if fcal != f:
+                        hdrj = DIC[j]['hdr']
+                        obstypec = hdrj['ESO PRO CATG']
+                        mjdc  = hdrj['MJD-OBS']
+                        bcd1c = hdrj['ESO INS BCD1 NAME']
+                        bcd2c = hdrj['ESO INS BCD2 NAME']
+                        chipc = hdrj['ESO DET CHIP NAME']
+                        ditc  = hdrj['ESO DET SEQ1 DIT']
+                        dif = mjd - mjdc
+                        absdif = np.abs(dif)
+
+                        if obstypec == 'CALIB_RAW_INT' and bcd1 == bcd1c and bcd2 == bcd2c and chip == chipc and dit == ditc:
+                            if absdif < timespan:
+                                #print(fcal)
+                                #print(mjdc)
+                                #print(dif)
+                                soffile.write('{} \t {} \n'.format(fcal,obstypec))
+                                calcount+=1
+
+                soffile.close()
+                print("Found",calcount,"suitable calibrators")
+
+            fmat = obstype[:-1]
+            if 'RAW_INT' not in fmat:
+                f = '#' + f
+                #print '{} \t {}'.format(f,fmat), ' added'
+        #except:
+         #   continue
+    return SOFFILE
+
+outdir = args.out_dir
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
 
 #---------------------------------------------------------------------------
 #---- Make the SOF files ---------------------------------------------------
-targsof = make_sof(args.targ_dir, args.out_dir, 'TARGET')
-calibsof = make_sof(args.calib_dir, args.out_dir, 'CALIB')
-combinedsof = '%s/%s.sof'%(args.out_dir, 'COMBINED')
-call('cat %s %s > %s'%(targsof, calibsof, combinedsof), shell=True)
-
-
+targsof = make_sof(args.in_dir, args.out_dir)
 
 
 #--------------------------------------------------------------------------
 #----- Run the Recipes ----------------------------------------------------
-sof = combinedsof
-outdir = args.out_dir
-#mat_cal_cphase = "esorex --output-dir=%s mat_cal_cphase %s"%(outdir,sof)
-#mat_cal_dphase = "esorex --output-dir=%s mat_cal_dphase %s"%(outdir,sof)
-#mat_cal_vis = "esorex --output-dir=%s mat_cal_vis %s"%(outdir,sof)
-mat_cal_oifits = "esorex --output-dir=%s mat_cal_oifits %s"%(outdir,sof)
+for isof in targsof:
+    print 'Running mat_cal_oifits on sof:%s'%(isof)
+    call("esorex --output-dir=%s mat_cal_oifits %s"%(outdir,isof), shell=True)
 
+    name, ext = os.path.splitext(isof)
+    #print(name)
 
-#print 'Running mat_cal_cphase on sof:%s'%(sof)
-#call(mat_cal_cphase, shell=True)
-#print 'Running mat_cal_dphase on sof:%s'%(sof)
-#call(mat_cal_dphase, shell=True)
-#print 'Running mat_cal_vis on sof:%s'%(sof)
-#call(mat_cal_vis, shell=True)
-print 'Running mat_cal_oifits on sof:%s'%(sof)
-call(mat_cal_oifits, shell=True)
-
-
-
-
+    # Rename files
+    resultFiles = glob.glob(outdir+'/TARGET_CAL_INT_????.fits')
+    #print(resultFiles)
+    for idx,fi in enumerate(resultFiles):
+        print("renaming",fi, name+"_"+str(idx+1)+'.fits')
+        os.rename(fi, name+"_"+str(idx)+'.fits')
+    #except:
+     #   print("WARNING: No product!")
