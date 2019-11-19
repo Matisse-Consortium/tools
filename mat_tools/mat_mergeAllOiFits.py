8e275476b23b51bbb1313e936026f932386937b7
Anthony Meilland <ame@matisse-oca.oca.eu>
2019-11-19 15:39:56 +0100

    mat_mergeAllOiFits.py

--- mat_tools/mat_mergeAllOiFits.py
+++ mat_tools/mat_mergeAllOiFits.py
@@ -15,7 +15,6 @@ from libPostTools import mat_mergeByTplStart
 
 
 if __name__ == '__main__':
-    print("boooo")
     parser = argparse.ArgumentParser(description='Merging all oifits files from the same template into a single OiFits file per detector.')
     parser.add_argument('dirIn', default="",help='Input directory')
     parser.add_argument('--dirOut', default="MERGED",help='Output directory')
