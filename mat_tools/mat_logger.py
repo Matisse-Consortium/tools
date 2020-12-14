15881b7c27624f6465cae7eed7568fb4c54846e2
AnthonyMeilland <63512040+AnthonyMeilland@users.noreply.github.com>
2020-12-14 09:49:37 +0100

    Update mat_logger.py

--- mat_tools/mat_logger.py
+++ mat_tools/mat_logger.py
@@ -202,11 +202,15 @@ class mat_logData():
                     isImageAcq = "T"
         elif self.tplid == "MATISSE_hyb_obs" or self.tplid == "MATISSE_hse_obs" or  \
              self.tplid == "MATISSE_hyb_obs_ft" or  self.tplid == "MATISSE_hyb_obs_ft_test" or \
-             self.tplid == "MATISSE_hyb_obs_vis" or  self.tplid == "MATISSE_hyb_obs_coh":
+            self.tplid == "MATISSE_hyb_obs_vis" or  self.tplid == "MATISSE_hyb_obs_coh":
             if  self.tplid == "MATISSE_hyb_obs" :
                 tpltype="OBS-HYB"
-            elif self.tplid == "MATISSE_hyb_obs_ft" or  "MATISSE_hyb_obs_ft_test" :
+            elif self.tplid == "MATISSE_hyb_obs_ft" or  "MATISSE_hyb_obs_ft_test":
                 tpltype="OBS-HYB-FT"
+            elif self.tplid == "MATISSE_hyb_obs_vis":
+                tpltype="OBS-HYB-VIS"
+            or  self.tplid == "MATISSE_hyb_obs_coh":
+                 tpltype="OBS-HYB-COH"
             else:
                 tpltype="OBS-HSE"
 
