ad7f42bb1f4079b004a4c1d6f90b86a08c101ff5
Fatme Allouche <fallouche@matisse-oca.oca.eu>
2021-02-09 14:48:08 +0100

    updating the code to take into account unchopped data

--- mat_tools/mat_showAcq.py
+++ mat_tools/mat_showAcq.py
@@ -330,7 +330,7 @@ if  __name__== '__main__' :
         parser.print_help()
         print("\n     Example : python mat_showAcq.py  MATIS.2019-11-07T06:25:21.184.fits")
         sys.exit(0)
-
+    print(args.filename)
     arg=sys.argv
     h=fits.open(args.filename)
     chopping=h[0].header['HIERARCH ESO ISS CHOP ST']
