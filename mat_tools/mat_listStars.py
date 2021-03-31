2561cef584e9edc6444021b9ef4811530299f61e
Florentin Millour <fmillour@oca.eu>
2021-03-31 06:04:06 +0200

    Update according to new rules

--- mat_tools/mat_listStars.py
+++ mat_tools/mat_listStars.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 This file is part of the Matisse pipeline GUI series
@@ -29,28 +29,32 @@ import numpy as np
 from astropy.io import fits
 import fnmatch
 from libAutoPipeline import matisseType
+from tqdm import tqdm
+import argparse
 
-RES = []
-obsTypes = ["TARGET_RAW", "CALIB_RAW","TARGET_RAW_INT", "CALIB_RAW_INT"]
 
-for root, dirs, files in os.walk("."):
-	print("Looking for observing files in "+root)
-        for files in fnmatch.filter(files, "*.fits*"):
-        
+def listStars(inDir):
+    RES = []
+    obsTypes = ["TARGET_RAW", "CALIB_RAW","TARGET_RAW_INT", "CALIB_RAW_INT"]
+
+    for root, dirs, files in os.walk(inDir):
+        print("Looking for observing files in "+root)
+        for files in tqdm(fnmatch.filter(files, "*.fits*"),unit=" file", unit_scale=False, desc="Working on"):
             header  = fits.open(os.path.join(root,files))[0].header
             types = matisseType(header)
             
             for typ in obsTypes:
                 if types == typ:
-          	    print(files)
+                   # print(files)
                     try:
                         starname  = header['HIERARCH ESO OBS TARG NAME']
                         dit  = header['HIERARCH ESO DET SEQ1 DIT']
                         chip = header["HIERARCH ESO DET CHIP NAME"]
                         mod  = header["HIERARCH ESO DET READ CURNAME"]
                     except:
-                        print("problem reading file")
-			continue
+                        #print("problem reading file")
+                        continue
+                    
                     if chip == "HAWAII-2RG":
                         try:
                             res     = header["HIERARCH ESO INS DIL NAME"]
@@ -68,9 +72,39 @@ for root, dirs, files in os.walk("."):
                         except:
                             print("problem reading N file")
                         
+                        
+    res, idx = np.unique(RES, return_index=True)
+    print("Stars:")
+    for i in res:
+        print(i)
+    return res;
 
-res, idx = np.unique(RES, return_index=True)
-print("Stars:")
-for i in res:
-	print(i)
 
+
+
+if __name__ == '__main__':
+    print("Starting mat_listStars...")
+
+    #--------------------------------------------------------------------------
+    parser = argparse.ArgumentParser(description='A small utility tool to list stars names in a data directory.')
+
+    #--------------------------------------------------------------------------
+    parser.add_argument('matFitsDir', default="",  \
+    help='The path to the file or directory containing your MATISSE fits data.')
+
+    try:
+        args = parser.parse_args()
+    except:
+        print("\n\033[93mRunning mat_listStars.py --help to be kind with you:\033[0m\n")
+        parser.print_help()
+        print("\n     Example : python mat_listStars.py /data/2018-05-19_OIFITS")
+        sys.exit(0)
+        
+    if os.path.isdir(args.matFitsDir):
+        print("Listing files in directory "+args.matFitsDir+"...")
+
+        listStars(args.matFitsDir);
+
+
+
+        
