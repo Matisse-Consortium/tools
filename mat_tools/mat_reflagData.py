baabf7b26076706472acd133e38ef416b06cd910
Florentin Millour <fmillour@oca.eu>
2021-07-07 12:04:38 +0200

    polish script

--- mat_tools/mat_reflagData.py
+++ mat_tools/mat_reflagData.py
@@ -24,6 +24,8 @@ knowledge of the CeCILL license and that you accept its terms.
 
 # Import stuff
 import os
+import argparse
+from fnmatch import fnmatch
 from tqdm import tqdm
 import astropy
 from astropy.io import fits
@@ -34,11 +36,12 @@ import glob
 
 ###############################################################################
 
-def reflagData(oifits):
-    print(filenames)
+def reflagData(oifits, keepOldFlags=0):
+    #print(oifits)
     
-    data=fits.open(filenames, mode='update')
+    data=fits.open(oifits, mode='update')
 
+    #print("reading data")
     WLEN = data['OI_WAVELENGTH'].data['EFF_WAVE']
 
     # Read VIS2 data
@@ -46,64 +49,75 @@ def reflagData(oifits):
     VIS2ERR  = data['OI_VIS2'].data['VIS2ERR']
     VIS2FLAG = data['OI_VIS2'].data['FLAG']
 
+    #print("Computing visibility")
     vis    = np.sqrt(np.abs(VIS2)) * np.sign(VIS2)
     viserr = 0.5* VIS2ERR / (vis+(vis==0));
 
-    wlmin = [2.94e-6, 4.5e-6, 8e-6];
-    wlmax = [4.1e-6,  5.0e-6, 12.5e-6];
+    wlmin = (2.94e-6, 4.5e-6, 8e-6);
+    wlmax = (4.1e-6,  5.0e-6, 12.5e-6);
+
+    # print("Getting old vis flags")
+    # if(keepOldFlags==1):
+    #flag = ~bool(VIS2FLAG);
+    # else:
+    #     flag = np.array(1,np.shape(VIS2FLAG));
     
-    flag = ~ VIS2FLAG;
-
-    flag = flag               &\ # Keep current flags
-        (VIS2 > 0. - VIS2ERR) &\ # V2 positive
-        (VIS2 < 1. + VIS2ERR) &\ # V2 < 1
-        (VIS2 > -0.1)         &\ # V2 positive - remove outliers
-        (VIS2 < 1.1)          &\ # V2 < 1 - remove outliers
-        (VIS2ERR > 0)         &\ # V2 error positive
-        (VIS2ERR < 0.1)       &\ # V2 errors < 0.1
-        (vis > 0. - viserr)   &\ # V positive
-        (vis < 1. + viserr)   &\ # V < 1
-        (vis > -0.1)          &\ # V positive - remove outliers
-        (vis < 1.1)           &\ # V < 1 - remove outliers
-        (viserr > 0)          &\ # V error positive
-        (viserr < 0.1)           # V errors < 0.1
-
+    #print("Computing new vis flags")
+    flag = (VIS2 > 0. - VIS2ERR) &\
+        (VIS2 < 1. + VIS2ERR) &\
+        (VIS2 > -0.1)         &\
+        (VIS2 < 1.1)          &\
+        (VIS2ERR > 0)         &\
+        (VIS2ERR < 0.1)       &\
+        (vis > 0. - viserr)   &\
+        (vis < 1. + viserr)   &\
+        (vis > -0.1)          &\
+        (vis < 1.1)           &\
+        (viserr > 0)          &\
+        (viserr < 0.1)
+           
+    #print("Filtering wavelength")
     # Filter interbands
-    for i,idx in enumerate(wlmin):
-        flag = flag             &\
-            (WLEN < wlmax[idx]) &\
-            (WLEN > wlmin[idx])
-    
+    wlmask = np.zeros(np.shape(WLEN),dtype=bool)
+    for i,wl in enumerate(wlmin):
+        wlmaski = ((WLEN < wlmax[i]) &\
+                   (WLEN > wlmin[i]))
+        wlmask = wlmask | wlmaski;
+    flag = flag & wlmask;
+        
+    #print("Inverting flags")
     # print(flag)
     flag = ~flag
 
+    #print("Setting flags")
     data['OI_VIS2'].data['FLAG'] = flag
     
+    #print("Getting CP flags")
     # Read CP data
     CP     = data['OI_T3'].data['T3PHI']
     CPERR  = data['OI_T3'].data['T3PHIERR']
     CPFLAG = data['OI_T3'].data['FLAG']
 
-    flag3 = ~ CPFLAG;
-
-    flag = flag          &\ # Keep current flags
-        (CPERR > 0.)     &\ # Closure phase error positive
-        (CPERR < 60)        # Closure phase error less than 1 radian
-
-    # Filter interbands
-    for i,idx in enumerate(wlmin):
-        flag3 = flag3             &\
-            (WLEN < wlmax[idx]) &\
-            (WLEN > wlmin[idx])
-
-    # Check the redundant closure phase
+    #if(keepOldFlags==1):
+    #    flag3 = ~CPFLAG;
+    #else:
+    #    flag3 = 1;
     
+    #print("Computing new flags")
+    flag3 = (CPERR > 0.) & (CPERR < 60)
+    flag3 = flag3 & wlmask;
+
+    #print("Redundant closure phase filtering")
+    # Check the redundant closure phase
+    fot i,cpi in enumerate()
            
+    #print("Inverting flag")
     # print(flag)
-    flag3 = ~ flag3;
+    flag3 = ~flag3;
 
     data['OI_T3'].data['FLAG'] = flag3;
     
+    #print("Saving fiel")
     data.flush();
     data.close();
     
@@ -162,7 +176,8 @@ if __name__ == '__main__':
                                  "CALIB_CAL_0*","RAW_DPHASE_*",
                                  "matis_eop*","nrjReal*",
                                  "DSPtarget*","nrjImag*",
-                                 "fringePeak*","BSreal*","BSimag*"]
+                                 "fringePeak*","BSreal*","BSimag*",
+                                 "*Image0.fits","*LFF.fits"]
             
             fifil = os.path.basename(fil)
             
@@ -170,28 +185,18 @@ if __name__ == '__main__':
                 #print(fifil)
                 for i in matchfilestoavoid:
                     if fnmatch(fifil, i):
-                        #print("Breeaaaaaak!")
+                        print("Breeaaaaaak!")
                         break;
                 
                 try:
                     #print("copying file to the right path...")
-                    hdu    = fits.getheader(fil)
-                    if hdu['HIERARCH ESO PRO CATG'] == 'CALIB_RAW_INT' or\
-                       hdu['HIERARCH ESO PRO CATG'] == 'TARGET_RAW_INT':
-                        copyfile(fil,
-                                 os.path.join(newdir,fifil))
-                        reflagData(os.path.join(newdir,fifil))
+                    copyfile(fil,
+                             os.path.join(newdir,fifil))
+                    #print("reflagging...",os.path.join(newdir,fifil))
+                    reflagData(os.path.join(newdir,fifil))
+                    #print("Done!");
                 except:
                     do_nothing = 1;
                     #print("Not a fits file!")
 
     print("I made my job!")
-
-
-    
-# Work on all FITS files in the current directory
-dir="*.fits"
-
-#print(glob.glob(dir))
-
-for filenames in glob.glob(dir):
