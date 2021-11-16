5dcc0a944a996cdcb5418789d95ba9f82248bf9d
Florentin Millour <fmillour@oca.eu>
2021-11-16 11:58:59 +0100

    add more dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,4 @@
 sudo apt-get install gtk+-3.0 python3-pip
 
-pip3 install tqdm astroquery openpyxl statistics
+pip3 install --user tqdm astroquery openpyxl statistics ObjectListView numpy matplotlib wxpython
 
