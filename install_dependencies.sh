033d422ff4f58dda8bfbfc291c31216936177a93
Florentin Millour <fmillour@oca.eu>
2022-09-17 19:49:02 +0200

    add one missing dependency in pip

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
 sudo apt-get install gtk+-3.0 python3-pip
 
-pip3 install --user tqdm astroquery openpyxl statistics ObjectListView numpy matplotlib wxpython shapely
+pip3 install --user attrdict tqdm astroquery openpyxl statistics ObjectListView numpy matplotlib wxpython shapely
 
