7de21801c724b0dc4e3ea72acdd2ca1de894f314
AlexisMatter <82473568+AlexisMatter@users.noreply.github.com>
2025-04-04 16:22:59 +0200

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install gtk+-3.0 python3-pip
 
-pip3 install --user attrdict tqdm astroquery openpyxl statistics ObjectListView numpy matplotlib shapely skycalc_cli
+pip3 install --user attrdict tqdm astroquery openpyxl statistics ObjectListView numpy matplotlib shapely wxpython skycalc_cli
 
